c3adf6c2521bd5500d50099942c1f1468c821e2b
ruagldh <golyshev.dmitry@gmail.com>
2024-10-20 00:51:58 +0300

    Apply mentor comments Add singleton

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,7 +3,11 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -104,7 +108,6 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Connection connection = Util.getConnection();
              Statement statement = connection.createStatement()) {
-
             statement.executeUpdate("TRUNCATE TABLE user");
         } catch (SQLException e) {
             throw new RuntimeException(e);
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -5,12 +5,28 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
+    private static volatile Connection connection;
     private static final String URL = "jdbc:mysql://localhost:3306/kata";
     private static final String USER = "root";
     private static final String PASSWORD = "1234567812";
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        if (connection == null || connection.isClosed()) {
+            synchronized (Util .class) {
+                if (connection == null || connection.isClosed()) {
+                    connection = DriverManager.getConnection(URL, USER, PASSWORD);
+                }
+            }
+        }
+        return connection;
     }
 
 }
